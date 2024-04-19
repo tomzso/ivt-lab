33e1f053ac00dba6e93b855990eb6f882d7a18f9
Le Ngoc Toan <99918734+tomzso@users.noreply.github.com>
2024-04-19 13:38:45 +0200

    Update GT4500.java

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -80,7 +80,10 @@ public class GT4500 implements SpaceShip {
         // try to fire both of the torpedo stores
         //TODO implement feature
 
-        firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
+        primaryTorpedoStore.fire(1);
+        secondaryTorpedoStore.fire(1);
+        
+        firingSuccess = true;
 
         break;
     }
